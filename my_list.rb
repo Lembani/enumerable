24852460586511582247b3350f73abf7aebd2aa8
Lembani Sakala <lembanisakala@gmail.com>
2022-07-15 10:53:33 +0200

    Add test inputs to verify solution

--- my_list.rb
+++ my_list.rb
@@ -6,3 +6,25 @@ class MyList
     @list = list
   end
 end
+
+# VERIFY SOLUTION
+
+# # Create our list
+# irb> list = MyList.new(1, 2, 3, 4)
+# => #<MyList: @list=[1, 2, 3, 4]>
+
+# # Test #all?
+# irb> list.all? {|e| e < 5}
+# => true
+# irb> list.all? {|e| e > 5}
+# => false
+
+# # Test #any?
+# irb> list.any? {|e| e == 2}
+# => true
+# irb> list.any? {|e| e == 5}
+# => false
+
+# # Test #filter
+# irb> list.filter {|e| e.even?}
+# => [2, 4]
