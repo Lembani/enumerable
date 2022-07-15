18d414cf92847f81bb983bdca6fcea62e5128090
onomeh <onwuanibeonome@gmail.com>
2022-07-15 10:07:34 +0000

    Create all method

--- my_enumerable.rb
+++ my_enumerable.rb
@@ -3,6 +3,19 @@ module MyEnumerable
   def each(&block)
     @list.each(&block)
   end
-
+  def all?(params = nil)
+    each do |item|
+      if block_given?
+        return false unless yield item
+      elsif params.is_a?(Class)
+        return false unless item.is_a?(params)
+      elsif params.is_a?(Regexp)
+        return false unless params.match(item)
+      else
+        return false unless item == params
+      end
+    end
+    true
+  end  
 end
-# rubocop:enable Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity
+# rubocop:enable Metrics/CyclomaticComplexity, Metrics/PerceivedComplexit
